b05857e0e4524591afdfcfa4c27880bcbfee578f
Thom van der Ven <thomvanderven@gmail.com>
2025-10-19 13:55:02 +0200

    Added null checking and default table for glossary

--- book/_static/glossary.js
+++ book/_static/glossary.js
@@ -1,3 +1,12 @@
+function checkNull(entry) {
+    if (entry === null) {
+        return "-"
+    }
+    else {
+        return entry
+    }
+}
+
 function load_glossary(filename, course) {
     fetch(filename)
         .then(response => response.json())
@@ -6,7 +15,7 @@ function load_glossary(filename, course) {
             tbody.innerHTML = ''; // Clear existing content
             data.forEach(row => {
                 const tr = document.createElement('tr');
-                tr.innerHTML = `<td>\\(${row.Symbol}\\)</td><td>${row.Name}</td><td>${row.Use}</td>`;
+                tr.innerHTML = `<td>\\(${checkNull(row.Symbol)}\\)</td><td>${checkNull(row.Name)}</td><td>${checkNull(row.Use)}</td>`;
                 tbody.appendChild(tr);
             });
 
@@ -15,28 +24,28 @@ function load_glossary(filename, course) {
             }
         })
         .catch(error => console.error('Error fetching glossary data:', error));
+}
+
+load_glossary('glossary_math.json', 'math_table');
+load_glossary('glossary_mechanics_thermodynamics.json', 'mechanics_thermodynamics');
+
+document.getElementById('glossary-select').addEventListener('change', function() {
+    var selectedValue = this.value;
+
+    var math_table = document.getElementById('math_table');
+    var mechanics_thermodynamics = document.getElementById('mechanics_thermodynamics');
+
+    switch (selectedValue) {
+        case 'glossary/1':
+            mechanics_thermodynamics.style.display = 'table';
+            math_table.style.display = 'none';
+            break;
+        case 'math':
+            math_table.style.display = 'table';
+            mechanics_thermodynamics.style.display = 'none';
+            break;
+        default:
+            mechanics_thermodynamics.style.display = 'none';
+            math_table.style.display = 'table';
     }
-
-    load_glossary('glossary_math.json', 'math_table');
-    load_glossary('glossary_mechanics_thermodynamics.json', 'mechanics_thermodynamics');
-
-    document.getElementById('glossary-select').addEventListener('change', function() {
-        var selectedValue = this.value;
-
-        var math_table = document.getElementById('math_table');
-        var mechanics_thermodynamics = document.getElementById('mechanics_thermodynamics');
-
-        switch (selectedValue) {
-            case 'glossary/1':
-                mechanics_thermodynamics.style.display = 'table';
-                math_table.style.display = 'none';
-                break;
-            case 'math':
-                math_table.style.display = 'table';
-                mechanics_thermodynamics.style.display = 'none';
-                break;
-            default:
-                mechanics_thermodynamics.style.display = 'none';
-                math_table.style.display = 'none';
-        }
-    });
+});
--- book/glossary/glossary.md
+++ book/glossary/glossary.md
@@ -1,3 +1,3 @@
 # Interactive Glossary
 
-<iframe src="../_static/glossary.html" width="100%" height="100%"></iframe>
+<iframe src="../_static/glossary.html" width="100%" height="500px"></iframe>
